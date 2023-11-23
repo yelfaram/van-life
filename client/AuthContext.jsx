575c8c2b46a780c0ca46522c55f2aa9982fa2d4d
Youssef Elfaramawy <yelfaram@gmail.com>
2023-11-23 18:16:26 -0500

    Minor tweak to authcontext

--- client/AuthContext.jsx
+++ client/AuthContext.jsx
@@ -1,25 +1,10 @@
-import { createContext, useContext, useState, useEffect } from "react"
+import { createContext, useContext, useState} from "react"
 import PropTypes from 'prop-types';
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
     const [loggedIn, setLoggedIn] =  useState(localStorage.getItem("loggedIn"))
-
-    useEffect(() => {
-        const handleStorageChange = () => {
-          console.log("Storage changed");
-          setLoggedIn(localStorage.getItem("loggedIn"));
-        }
-    
-        console.log("Adding storage event listener");
-        window.addEventListener('storage', handleStorageChange);
-    
-        return () => {
-          console.log("Removing storage event listener");
-          window.removeEventListener('storage', handleStorageChange);
-        }
-      }, [])
     
     const login = () => {
         setLoggedIn(true)
